98a8a11820ef933803716021d76a41093a490633
Chris Ricci <riccic@Chriss-MacBook-Pro.local>
2017-06-12 14:11:26 -0400

    fixing app.py overwrite

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests
 import os
 import sys
 import logging
-import datetime
 
 from flask import Flask
 from flask_prometheus import monitor
@@ -12,11 +11,22 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.DEBUG)
 
+greeterUrl = os.environ.get('GREETERSERVICE') if os.environ.get('GREETERSERVICE') != None else 'http://localhost:8080'
+app.logger.debug('GREETERSERVICE: ' + greeterUrl)
+
+nameserviceUrl = os.environ.get('NAMESERVICE') if os.environ.get('NAMESERVICE') != None else 'http://localhost:8081'
+app.logger.debug('NAMESERVICE: ' + nameserviceUrl)
+
 @app.route('/')
 def index():
     # Call Greeter Service
-    timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))	
-    return timestamp + " Hello, World!\n"
+    greeterResponse = requests.get(greeterUrl)
+    app.logger.debug('GREETER-RESPONSE: ' + greeterResponse.text)
+
+    # Call Name Service
+    nameserviceResponse = requests.get(nameserviceUrl) 
+    app.logger.debug('NAME-SERVICE: ' + nameserviceUrl)
+    return "%s, %s!" % (greeterResponse.text, nameserviceResponse.text)
 
 if __name__ == '__main__':
     monitor(app, port=8000)
