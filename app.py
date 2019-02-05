47f1ed80b0f2a06433068c2aeed7dff722d9fa92
cricci82 <chris.ricci82@gmail.com>
2019-02-05 13:53:44 -0500

    updating

--- app.py
+++ app.py
@@ -119,12 +119,12 @@ def getForwardHeaders(request):
 
 @app.route('/')
 def index():
+    # do_cool_stuff()
     status, greeting = getGreeting()
     status, name = getName() 
     timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    # time.sleep(int(2))
-    do_cool_stuff()
-    return "%s %s, %s! -- Update\n" % (timestamp, greeting, name)
+
+    return "%s %s, %s!\n" % (timestamp, greeting, name)
 
 @trace()
 def getGreeting():
